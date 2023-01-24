e6abcc1044dcf78ea5b84f791991a9b052f9039d
Bianca Vieira <vbieira@outlook.com>
2023-01-24 00:37:08 -0300

    create anki translator gem

---
+++ lib/anki_translator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "anki_translator/version"
+
+module AnkiTranslator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/anki_translator/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AnkiTranslator
+  VERSION = "0.1.0"
+end
