468ad51567e1a25e73a72567f8050c36f9dfdcaf
Bianca Vieira <vbieira@outlook.com>
2023-04-27 02:02:59 -0300

    move dotenv require to configure

--- lib/anki_translator.rb
+++ lib/anki_translator.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require "dotenv"
 require "csv"
 require "faraday"
 require "capybara/sessionkeeper"
@@ -13,8 +12,6 @@ require_relative "anki_translator/references/macmillan_dictionary"
 require_relative "anki_translator/references/google_translate"
 require_relative "anki_translator/references/merriam_webster"
 require_relative "anki_translator/version"
-
-Dotenv.load
 
 module AnkiTranslator
   class Error < StandardError; end
